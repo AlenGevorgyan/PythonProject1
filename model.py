9162347574a1871b1f329f7fd85d524fa9219ad3
Admin <alengevorgyan2009@gmail.com>
2025-06-29 21:37:59 +0400

    dataset added

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-import cv2
+import kagglehub
 
-img = cv2.imread(r"C:\Users\Admin\.cache\kagglehub\datasets\grassknoted\asl-alphabet\versions\1\asl_alphabet_train\asl_alphabet_train\B\B1.jpg")
-print("Image shape:", img.shape)  # (height, width, channels)
-print("Height:", img.shape[0])
-print("Width:", img.shape[1])
+# Download latest version
+path = kagglehub.dataset_download("grassknoted/asl-alphabet")
+
+print("Path to dataset files:", path)
